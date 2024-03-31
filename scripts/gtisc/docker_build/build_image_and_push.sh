f0de50f21d86af02d130e1a38c5a17cd9e7c0e35
yype <yype@foxmail.com>
2024-03-31 02:08:46 -0400

    fix: use nocache and remove url adds

--- scripts/gtisc/docker_build/build_image_and_push.sh
+++ scripts/gtisc/docker_build/build_image_and_push.sh
@@ -1,4 +1,4 @@
-docker build . -t super.gtisc.gatech.edu/s2e:latest
+docker build --no-cache -t super.gtisc.gatech.edu/s2e:latest .
 
 docker login super.gtisc.gatech.edu --username=gtisc --password=Dock3r_reg1stry_f0r_Gt1sc
 docker push super.gtisc.gatech.edu/s2e:latest
