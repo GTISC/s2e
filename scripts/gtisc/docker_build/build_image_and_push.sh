066541ff9df8730322fd109f584196b4a4e42d1e
yype <yype@foxmail.com>
2024-03-31 23:29:02 -0400

    fix: using github secrets & env vars

--- scripts/gtisc/docker_build/build_image_and_push.sh
+++ scripts/gtisc/docker_build/build_image_and_push.sh
@@ -1,5 +1,15 @@
+if [[ -z "${DOCKER_REGISTRY_USER}" ]]; then
+  echo "Error: Please set DOCKER_REGISTRY_USER."
+  exit 1
+fi
+
+if [[ -z "${DOCKER_REGISTRY_PASS}" ]]; then
+  echo "Error: Please set DOCKER_REGISTRY_PASS."
+  exit 1
+fi
+
 docker build --no-cache -t super.gtisc.gatech.edu/s2e:latest .
 
-docker login super.gtisc.gatech.edu --username=gtisc --password=Dock3r_reg1stry_f0r_Gt1sc
+docker login super.gtisc.gatech.edu --username=$DOCKER_REGISTRY_USER --password=$DOCKER_REGISTRY_PASS
 docker push super.gtisc.gatech.edu/s2e:latest
 docker logout super.gtisc.gatech.edu
